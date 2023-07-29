dc903a1a9fc74ae787775f47f726b61621856534
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-30 00:53:45 +0800

    [Feature 2] Admin and Client Constraint

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,12 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_admin 
+
+  def index
+    @users = User.page params[:page]
+  end
+
+  def check_admin
+    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  devise_for :users
+  constraints(AdminDomainConstraint.new) do
+    namespace :admin do
+      resources :users
+    end
+  end
+
+  constraints(ClientDomainConstraint.new) do
+    resources :posts do
+      resources :comments, except: :show
+    end
+  end
 end
