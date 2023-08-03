57e309080ccc77722c009c5b0a4e79078a4a3e4b
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-03 17:40:30 +0800

    [Feature 5] Get opendata from [Feature 5] Get opendata from ph govermentph goverment

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'phonelib'
 
 gem 'carrierwave'
 
+gem 'rest-client'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.6"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end    
+      resources :provinces, only: %i[index show], defaults: { format: :json } do
+        resources :cities, only: :index, defaults: { format: :json }
+      end
+      resources :cities, only: %i[index show], defaults: { format: :json } do
+        resources :barangays, only: :index, defaults: { format: :json }
+      end
+      resources :barangays, only: %i[index show], defaults: { format: :json }
+    end
+  end
 
 
   constraints(AdminDomainConstraint.new) do
@@ -23,5 +36,6 @@ Rails.application.routes.draw do
         root 'home#index'
         resources :home
         resource :user
+
     end
 end
