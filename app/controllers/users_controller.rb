e54d51b68586d6a1be1881989a324584582f6c5f
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-07 11:28:09 +0800

    [Feature 4] Fix User Show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def show
+    @user=current_user
+  end
+
+
+  def edit
+    @user=current_user
+  end
+
+end
