33ed1e7cb2b99e9c504c25d3f52a3486f0d000e3
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-08 00:36:40 +0800

    [Feature 2] Admin Log-In Constraint

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,9 +1,3 @@
 class Admin::HomeController < ApplicationController
- before_action :check_admin
   def index; end
-
-  private
-  def check_admin
-    redirect_to ('Not Found') unless current_user.admin?
-  end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,17 +1,28 @@
 class Admin::SessionsController < Devise::SessionsController
-
+    before_action :admin_check, only: [:create]
     def new
-        super
-      end
+      super
+    end
     
-      # POST /clients/sign_in
-      def create
+    # POST /clients/sign_in
+    def create
+      if @user && @user.admin?
         super
-      end
-    
-      # DELETE /clients/sign_out
-      def destroy
-        super
-      end
+    else
+      flash[:alert] = "You don't have permission to access this page."
+      redirect_to new_user_session_path
+    end
+    end
+  
+    # DELETE /clients/sign_out
+    def destroy
+      super
+    end
+
+    private
+
+    def admin_check
+      @user = User.find_by_email(params[:user][:email])
+    end
 end
 
