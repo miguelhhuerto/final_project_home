b0167d0836db5ac7e5501c57904f4eba2253e8ec
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-08 20:19:55 +0800

    [Feature 11] Item Soft Delete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
     belongs_to :user
+    default_scope { where(deleted_at: nil) }
+
+    def destroy
+        update(deleted_at: Time.current)
+     end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_114407) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_121703) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_114407) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
