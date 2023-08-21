0fa2a9bb42507e0abce52c1f6030e7d029dcaca9
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-21 20:16:03 +0800

    [Feature 24] Claim Prize

--- app/controllers/winnings_controller.rb
+++ app/controllers/winnings_controller.rb
@@ -1,5 +1,19 @@
 class WinningsController < ApplicationController
+    before_action :set_winner, only: [:claim]
     def index
         @winners = Winner.where(user_id: current_user.id)
+    end
+
+    def claim
+      if @winner.may_claim? && @winner.claim!
+        flash[:notice] = "Item Claimed!"
+        redirect_to user_path 
+      end
+    end
+
+    private
+
+    def set_winner
+      @winner = Winner.find(params[:id])
     end
 end
