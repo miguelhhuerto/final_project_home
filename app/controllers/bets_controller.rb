c6be5b93514954b53cfee04d642becc38e7da076
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-21 17:30:58 +0800

    [Feature 15] Bet Controller

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -9,11 +9,25 @@ class BetsController < ApplicationController
         bet = Bet.create(user: current_user, item: @item)
         if bet.save
           Rails.logger.info "Bet created: #{bet.inspect}"
+          if @item.batch_count == 1
+            number_count = format('%04d',@item.bets.count)
+          else
+            number_count = format('%04d',  ( @item.bets.count - ((@item.minimum_bets)*(@item.batch_count) )))
+          end
+    
+          formatted_date = Time.current.strftime('%y%m%d')
+          item_id = @item.id
+          batch_count = @item.batch_count
+    
+          serial_number = "#{formatted_date}-#{item_id}-#{batch_count}-#{number_count}"
+          bet.update(serial_number: serial_number)
         else
           Rails.logger.error "Bet creation failed: #{bet.errors.full_messages}"
           redirect_to lottery_index_path, alert: "Bet creation failed: #{bet.errors.full_messages.to_sentence}"
           return
         end
+
+
       end
       current_user.decrement!(:coins, coins_to_bet)
       redirect_to lottery_index_path, notice: "#{coins_to_bet} bets placed successfully."
@@ -21,7 +35,4 @@ class BetsController < ApplicationController
       redirect_to lottery_index_path, alert: "Insufficient coins"
     end
   end
-
-
-
 end
