8069abdeccd1841a280f1062b5e6f899c5cf313e
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-23 15:12:42 +0800

    [Feature 28] Bonus

---
+++ app/controllers/admin/bonus_controller.rb
@@ -0,0 +1,24 @@
+class Admin::BonusController < ApplicationController
+    def new;
+        @order = Order.new
+        @admin_balance_view = true
+    end
+
+    def create
+        @order = Order.new(order_params)
+        if @order.save
+          if @order.may_submit?
+            @order.submit!
+            if @order.may_pay?
+                @order.pay!
+            end
+          end
+        end 
+          @order.user.increment!(:coins, @order.coins )
+          flash[:notice] = 'Order created successfully'
+          redirect_to new_admin_user_orders_increase_path(user.id)
+        else
+          flash.now[:alert] = 'Order create failed'
+          render :new, status: :unprocessable_entity
+    end
+end
