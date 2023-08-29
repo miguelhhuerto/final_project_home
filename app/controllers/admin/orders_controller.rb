c894969c631e7620dc91d08f2a9ce9c4e92fbc88
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-29 17:03:13 +0800

    [Feature 20] Order Display Columns

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,47 @@
 class Admin::OrdersController < ApplicationController
-    before_action :set_order, only: [:submit, :pause, :cancel]
+    before_action :set_order, only: [:pay,:submit, :cancel]
+    before_action :set_user, only: [:new, :create, :cancel]
     def index
         @orders = Order.all
+  
     end
+    
+    # def new
+    #   @user = User.find(params[:user_id])
+    #   puts "User ID: #{params[:user_id]}, Found User: #{@user}"
+    #   @order = Order.new
 
+    # end
+
+    # def create
+    #   @user = User.find_by(username: params[:user_id])
+    #   puts "Creating order..."
+    #   @order = Order.new(order_params)
+    
+    #   if @order.save
+    #     puts "Order saved successfully."
+    #     redirect_to admin_users_path(@user), notice: 'Order created successfully.'
+    #   else
+    #     puts "Failed to save order."
+    #     flash.now[:alert] = "Failed to create order: #{@order.errors.full_messages.join(', ')}"
+    #     redirect_to admin_users_path(@user)
+    #   end
+    # end
+
+    def pay
+      if @order.may_pay? && @order.pay!
+        flash[:notice] = "Order Paid!"
+        redirect_to admin_orders_path 
+      end
+    end
+    
+    def submit
+      if @order.may_submit? && @order.submit!
+        flash[:notice] = "Order Submitted"
+        redirect_to admin_orders_path 
+      end
+    end
+  
     def cancel
       if @order.may_cancel? && @order.cancel!
         flash[:notice] = "Order Canceled"
@@ -11,11 +49,17 @@ class Admin::OrdersController < ApplicationController
       end
     end
 
-   
   private
+
+  def order_params
+    params.require(:order).permit(:coins, :genre, :remarks, :user_id)
+  end
 
   def set_order
     @order = Order.find(params[:id])
   end
 
+  def set_user
+    @user = User.find_by(username: params[:user_id])
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,14 @@
 class Order < ApplicationRecord
+    attr_accessor :admin_balance_view
     belongs_to :user
     belongs_to :offer, optional: true
     enum genre: { deposit: 0, increase: 1, deduct: 2, bonus: 3, share: 4}
-    validate :amount_for_genre  
-    validates :offer, presence: true, if: :deposit_genre?
+    validates :amount, numericality: {greater_than: 0}, if: :deposit?
+    validates :offer, presence: true, if: :deposit?
+    validates :coins, numericality: {greater_than: 0}
     after_create :assign_serial_number
-    validates :remarks, presence: true, if: -> { @admin_balance_view }
+    validates :remarks, presence: true, if: -> { self.admin_balance_view }
+
 
 include AASM
 
@@ -14,66 +17,67 @@ include AASM
       state :submitted, :canceled, :paid
 
     event :submit do
-      transitions from: :pending, to: :submitted, guard: :allow_transition?, success: :revise_quantity_and_batch_count
+      transitions from: :pending, to: :submitted
     end
 
     event :cancel do
-      transitions from: [:pending, :submitted, :paid], to: :canceled, guard: :allow_transition?,success: :change_coins_canceled
+      transitions from: [:pending, :submitted], to: :canceled
+      transitions from: :paid, to: :canceled, guard: :can_return_user_coins?, success: [:decrease_coins_canceled,:change_coins_canceled]
     end
 
     event :pay do
-      transitions from: :submitted, to: :paid, guard: :before_ended?, success: :change_coins_paid
+      transitions from: :submitted, to: :paid, guard: :deposit?, success: [:increase_total_deposit,:change_coins_paid]
+      transitions from: :pending, to: :paid, guard: :can_deduct_user_coins?, success: :change_coins_paid
     end
 
+  end
+
+
+  def can_deduct_user_coins?
+    return true if ( !deduct? || (deduct? && user.coins >= coins))
+    errors.add(:base, "User Coins Not Sufficient")
   end
 
   def assign_serial_number
+    
+    formatted_date = Time.current.strftime('%y%m%d')
+    order_id = self.id
     number_count = user.orders.count
-    serial_number = "#{Time.current}-#{order.id}-#{user_id}-#{number_count}"
+    serial_number = "#{formatted_date}-#{order_id}-#{number_count}"
+    
     self.update(serial_number: serial_number)
   end
 
-  def change_coins_paid
-    return unless status == 'submitted'
-
-    if self.genre == 'deposit'
+  def increase_total_deposit
+    if deposit?
       user.increment!(:total_deposit, amount)
     end
+  end
 
-    if self.genre == 'deduct'
-      user.decrement!(:coin, amount)
+  def change_coins_paid
+    if deduct?
+      user.decrement!(:coins, coins)
     else
-      user.increment!(:coin, amount)
+      user.increment!(:coins, coins)
+    end
+  end
+
+  def decrease_coins_canceled
+    if deposit?
+      user.decrement!(:total_deposit, amount)
     end
   end
 
   def change_coins_canceled
-    return unless status == 'paid'
-
-    if self.genre == 'deposit'
-      user.decrement!(:total_deposit, amount)
-    end
-
-    if self.genre != 'deduct'
-      user.decrement!(:coin, amount)
+    if !deduct?
+      user.decrement!(:coins, coins)
     else
-      user.increment!(:coin, amount)
+      user.increment!(:coins, coins)
     end
   end
 
-  def allow_transition?
-    user.coins >= amount
-  end
-
-  private
-
-  def amount_for_genre
-    if genre != 'deposit' && (amount.present? && amount <= 0)
-      errors.add(:amount, "must be greater than 0 for non-deposit genre")
-    end
-  end
-
-  def deposit_genre?
-    genre == 'deposit'
+  def can_return_user_coins?
+    return true if (!deduct? && user.coins >= coins) || deduct?
+    errors.add(:base, "User Coins Not Sufficient")
   end
 end
