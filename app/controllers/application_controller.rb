619543bc7e52a7055c6bdf5251e4c53b4fc5dd9d
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-29 17:00:22 +0800

    Interntionalization Set Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+  
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
