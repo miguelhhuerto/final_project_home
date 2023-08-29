d2abbd5537389217013568f359229b002a17e1bf
miguelhhuerto <miguelhhuerto@gmail.com>
2023-08-30 02:12:12 +0800

    [Feature 34 &35] Client Homepage View

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @banners = Banner.all
+    @feedbacks = Feedback.all
+    @items = Item.where(status: "active", state: "started")
+    @news_tickers = NewsTicker.where(status: "active")
   end
 end
